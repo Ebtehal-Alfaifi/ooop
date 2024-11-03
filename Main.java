86e4f69981a3e4b07778a6d23a28e321f5d471b0
Ebtehal Alfaifi <ebtehal.alfaifi@outlook.sa>
2024-11-03 18:07:06 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Employee e2=new Employee("1190865412"," jana ",7000);
+        System.out.println(" your name is "+e2.getId());
+        System.out.println( " your name is "+e2.getName());
+        System.out.println("your sallery is " +e2.getSallery());
+        System.out.println("************************************");
+
+
+        System.out.println(" your sallery after incresing"+e2.increse(10));
+
+        System.out.println("************************************************");
+        System.out.println();
+        System.out.println("your annual sallery is "+e2.getAnnualSalary());
+
+    }}
